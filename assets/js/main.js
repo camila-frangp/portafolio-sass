b14b0ba38fb9c882dc255642d3b84b50f78511dd
camila-frangp <camila.frangp@gmail.com>
2017-07-23 19:02:18 -0400

    Estilos de div portafolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
 	$('.slider').slider();
 	$('.collapsible').collapsible();
 
+	/*$(".img1").hover(function(){
+		$(this).css("background-color","rgba(0,0,0,0.8)");
+		$(this).css("z-index","1");
+	})*/
 });
 
 
