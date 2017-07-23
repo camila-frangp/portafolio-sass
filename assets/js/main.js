702eb27afd91806719c0b89ff6fe0287ee83c706
camila-frangp <camila.frangp@gmail.com>
2017-07-23 17:23:20 -0400

    Estilos de header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	$(".button-collapse").sideNav();
 	$('.dropdown-button').dropdown({hover: true});
-	      $('.slider').slider();
+	$('.slider').slider();
+	$('.collapsible').collapsible();
 
 });
 
